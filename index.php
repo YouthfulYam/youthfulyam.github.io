2daef11a666f1ca48269d50664a0ea9455614e30
YouthfulYam <149662241+YouthfulYam@users.noreply.github.com>
2024-01-23 14:04:52 +1100

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -10,28 +10,9 @@
 </head>
 
 <body>
-    <header>
-        <nav>
-            <div class="logo">
-                <a href="index.html"><img src="images/nav-img.png" alt="nav-img" class="nav-img"></a> 
-                <!--//https://mentalmodels4life.net/2017/03/19/pixar-on-management/-->
-            </div>
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li class="dropdown">
-                    <button class="dropbtn"><a href="product.html">Products</a></button>
-                    <div class="dropdown-content">
-                        <a href="vacuum_robot.html">Vacuums</a>
-                        <a href="lawnmower_robot.html">Lawn mowers</a>
-                        <a href="pool_robot.html">Pool cleaners</a>
-                    </div>
-                </li>
-                <li><a href="enquire.html">Enquire</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="enhancements.html">Enhancements</a></li>
-            </ul> 
-        </nav>
-    </header>
+<?php
+    include('header.inc');
+?>
 
     
 <div class="container">
@@ -154,9 +135,9 @@
         
     </main>
 
-    <footer>
-        <p class="foo">&copy; 2023 Autonomous Robots Store. All rights reserved.</p>
-    </footer>
+<?php
+    include('footer.inc');
+?>
 </body>
 
 </html>
