2dcbdbd7c815a40ec33819a89398261081805a36
Dacelo404 <46417757+Dacelo404@users.noreply.github.com>
2024-01-30 17:26:01 +1100

    Error page upload
    Errors link to this page

---
+++ error_page.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error Page</title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="styles/enquire.css">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://use.typekit.net/hja7nrk.css">
+    <!-- Icons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  
+</head>
+
+<?php
+    include('header.inc');
+?>
+
+<body>
+    <div class="error-container">
+        <h1>Error</h1>
+        <p>Access to this page is restricted.</p>
+        <p><a href="index.php">Go back to the homepage</a></p>
+    </div>
+</body>
+</html>
